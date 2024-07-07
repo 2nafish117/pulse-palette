68f3ac74decc9f96b3ff10db59144cca9f487bef
shashank <shashank.shashank.c@gmail.com>
2024-07-07 19:23:04 +0530

    rename desktop_client to test_client, update back submodule

--- scripts/test_client_buildrun.bat
+++ scripts/test_client_buildrun.bat
@@ -1,9 +1,9 @@
 @echo off
 
 echo "Building Client..."
-odin build client/ -out:bin/client.exe -vet-semicolon -debug -collection:soln=.
+odin build test_client/ -out:bin/test_client.exe -vet-semicolon -debug -collection:soln=.
 echo "Done Building Client..."
 
 if %errorlevel% neq 0 exit echo Build failed. && /b %errorlevel%
 
-"bin/client.exe"
+"bin/test_client.exe"
